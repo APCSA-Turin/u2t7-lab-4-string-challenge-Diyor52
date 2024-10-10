4b6286622f1691168e87f7fd55dcf4f2078c8ab4
Diyor52 <diyora2@nycstudents.net>
2024-10-10 16:30:06 +0000

    Diyor lab 4

---
+++ src/main/java/com/example/project/Main.java
@@ -0,0 +1,7 @@
+package com.example.project;
+public class Main {
+    public static void main(String [] args){
+        StringProblems methods = new StringProblems();
+        System.out.println(methods.conCat("abc", ""));
+    }
+} 
